0f093de965bfc0971ea5fb809e2cb74bdd2b2a9f
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-01-24 16:29:02 +0100

    chore: update of gsap animations compleded

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 gsap.from('.nav-container', { duration: 1, y: '-100%', ease: 'bounce' });
+gsap.from('.links', { duration: 1, opacity: 0, delay: 1, stagger: 0.5 });
+gsap.from('.left', { duration: 1, x: '-100vw' });
+gsap.from('.right', { duration: 2, x: '-100vw', ease: 'power2.in', delay: 1 });
 
-gsap.from('.links', { duration: 1, opacity: 0, stagger: 5, delay: 1 });
+gsap.to('.footer', {
+  duration: 1,
+  y: 0,
+  ease: 'elastic',
+  delay: 1,
+});
+
+gsap.fromTo(
+  '.button button',
+  {
+    y: 0,
+    ease: 'elastic',
+    rotation: 760,
+    scale: 0,
+    opacity: 0,
+  },
+  {
+    duration: 1,
+    y: 0,
+    ease: 'elastic',
+    delay: 1,
+    rotation: 0,
+    scale: 1,
+    opacity: 1,
+  }
+);
